6acff26201f1608728dbe957914b570866c481fd
xeada77 <luismartq@gmail.com>
2023-07-12 00:11:35 +0200

    11 julio pc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,61 @@
+import { 
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom"
+
+import Home from "./pages/home/Home"
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+
+import "./styles/global.scss"
+
 function App() {
 
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        
+        <Footer />
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (<Layout />),
+      children: [
+        {
+          path: "/",
+          element: (<Home />),
+        },
+        {
+          path: "/users",
+          element: (<Users />),
+        },
+        {
+          path: "/products",
+          element: (<Products />),
+        },
+      ]
+    },
+  ]);
+
   return (
-    <div>Hello World</div>
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,27 @@
+import "./navbar.scss"
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="logo" />
+        <span>lamadmin</span>
+      </div>
+      <div className="icons">
+        <img src="search.svg" alt="" className="icon"/>
+        <img src="app.svg" alt="" className="icon"/>
+        <img src="expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="notifications.svg" alt="" className="icon" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="https://images.pexels.com/photos/15591154/pexels-photo-15591154/free-photo-of-nieve-ciudad-moda-rojo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+          <span>Jane</span>
+        </div>
+        <img src="settings.svg" alt="" className="icon"/>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
